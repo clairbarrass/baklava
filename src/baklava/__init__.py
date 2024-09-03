837f36559407b4c02da2ea12c1cb2e51dffc497c
Clair Barrass <clair.barrass@ed.ac.uk>
2024-09-03 14:04:18 +0100

    first

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "ClairB"
+__email__ = "c.barrass@epcc.ed.ac.uk"
+__version__ = "0.1.0"
